cb23101eb72996a9a8cbf5b2478d1248cc562201
Apal <apalisdonatty@gmail.com>
2019-07-12 21:38:33 -0300

    Initial commit

---
+++ src/test/java/com/mercadolibre/orbit/OrbitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mercadolibre.orbit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrbitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
