a97b195d1fbac40871f8a07152aefc13764e2ece
Apal <apalisdonatty@gmail.com>
2019-07-21 12:08:35 -0300

    -Add: OrbitcalculationJobRunner | @Scheduled annotation to run a cron everyday for updating orbits

--- src/main/java/com/mercadolibre/orbit/app/job/OrbitCalculationJobRunner.java
+++ src/main/java/com/mercadolibre/orbit/app/job/OrbitCalculationJobRunner.java
@@ -17,6 +17,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
@@ -52,7 +53,7 @@ public class OrbitCalculationJobRunner {
      *
      * @return
      */
-    //@Scheduled(cron = "0 0 12 * * *")
+    @Scheduled(cron = "0 0 12 * * *")
     @Async("processExecutor")
     public void asyncTaskCalculateOrbitStatus(OrbitCalculationJob job) throws JobStillRunningRuntimeException {
         // Check if there is no Job still running (JobStatus.CREATED)
